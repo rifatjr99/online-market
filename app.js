f35941a371bf34e43ac6fec5d98731d15c8c0b3e
rifatjr99 <rifatkhan4054ff@gmial.com>
2023-05-13 08:23:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const rifat=()=>{
+document.getElementById('sidenav').style.display='block';
+document.getElementById('sidenav').style.position='fixed';
+}
+
+
+const hideNav=()=>{
+    document.getElementById('sidenav').style.display='none';
+}
+
+
+
+const them=()=>{
+    document.getElementById('demos').style.background='#0f0f0f67';
+}
+
